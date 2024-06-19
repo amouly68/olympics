8cba26f01a957485ac68993be1bbc2fd5245370f
Antoine Mouly <amouly@k0r3p4.42mulhouse.fr>
2024-06-19 13:31:54 +0200

    data

--- scrapping.py
+++ scrapping.py
@@ -5,8 +5,15 @@ import pandas as pd
 
 
 medals = pd.read_csv('medals.csv')
-olympics = pd.read_csv('olympics.csv')
+athletes = pd.read_csv('athlete_events.csv')
+regions = pd.read_csv('noc_regions.csv')
+
+
+
+athletes.describe() 
+
 
 medals.head()
-olympics.head()
+athletes.head()
+regions.head()
 
