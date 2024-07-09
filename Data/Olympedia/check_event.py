b31cf7e7598ff80a5b4e14e30a77235f8046db6a
amouly <amoule@mail.com>
2024-07-09 14:24:13 +0200

    a faier

--- Data/Olympedia/check_event.py
+++ Data/Olympedia/check_event.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Read the CSV file
 data = pd.read_csv('Olympic_Athlete_Event_Results.csv')
+data.head()
 test_riner = data[data['athlete_id'] == 112724]
 print (test_riner)
 
@@ -16,3 +17,13 @@ data_results.head()
 test_results = data_results[data_results['result_id'].isin([260537, 316678, 354880, 18001172, 18000780])]
 with open('test.txt', 'a') as f:
     test_results.to_string(f, index=False)
+
+
+data_results.describe()
+data_results.info()
+
+
+games = pd.read_csv('Olympics_Games.csv')
+
+
+
